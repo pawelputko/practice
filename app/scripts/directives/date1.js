623151a5a687add50b02cce6e5e2395116535a36
root <pawelputko@gmail.com>
2016-03-14 13:47:06 +0100

    Date done

--- app/scripts/directives/date1.js
+++ app/scripts/directives/date1.js
@@ -4,12 +4,14 @@
 angular.module('practiceApp')
   	.directive('dateA', function () {
     	return {
-    		template: '<span>{{date}}</span>',
-      		restrict: 'E',
-      		scope: {},
-      		replace: true,
-      		controller: function ($scope) {
-				$scope.date = moment().startOf('hour').fromNow();
+    		template: '<span>{{date}}<span>',
+      	restrict: 'E',
+      	scope: {
+            momentFormat: '='
+          },
+      	replace: true,
+      	controller: function ($scope) {
+				    $scope.date = moment($scope).format($scope.momentFormat);
       		}
       	};
     });
